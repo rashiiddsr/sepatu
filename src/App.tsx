15c88f06e5506fe6e64a67c6a35eabeaa4922a5a
Rashid <74960869+rashiiddsr@users.noreply.github.com>
2026-01-12 03:48:28 +0700

    Fix product detail route

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
             <Route path="/products/:id" element={<ProductDetail />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
